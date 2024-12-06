3c19480c1926b205cbae357c78cbd2bec5b320c5
Muhammed Maklad <mohammedmaklad97@gmail.com>
2024-12-06 23:18:22 +0200

    BlockProvider

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,13 +1,31 @@
+import 'package:breakingbadproject/business_logic/bloc/characters_cubit.dart';
 import 'package:breakingbadproject/constants/strings.dart';
+import 'package:breakingbadproject/data/repository/character_repository.dart';
 import 'package:breakingbadproject/presentation/screens/characterDetails.dart';
 import 'package:breakingbadproject/presentation/screens/characters.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AppRouter{
-  Route? generateRoute(RouteSettings settings){
-    switch (settings.name){
+import 'data/services/character_web_services.dart';
+
+class AppRouter {
+  late CharactersCubit charactersCubit;
+  late CharacterRepository characterRepository;
+
+  AppBar() {
+    characterRepository = new CharacterRepository(CharacterWebServices());
+    charactersCubit = new CharactersCubit(characterRepository);
+  }
+
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
       case characterScreen:
-        return MaterialPageRoute(builder: (_) => CharactersScreen());
+        return MaterialPageRoute(builder: (_) =>
+            BlocProvider(create: (BuildContext context) =>
+                charactersCubit
+              , child: CharactersScreen()
+              ,)
+        );
       case characterDetailsScreen:
         return MaterialPageRoute(builder: (_) => CharacterDetailsScreen());
     }
